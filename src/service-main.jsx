bc88f18a26222a8bdd8104c16f6620c0df9d1da7
Aaron Espinosa <aaron@esencial.work>
2025-12-28 15:06:30 -0600

    feat: add PWA install banner to service app

--- src/service-main.jsx
+++ src/service-main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Backstage from './pages/Backstage'
+import PWAInstallBanner from './components/PWAInstallBanner'
 
 createRoot(document.getElementById('root-service')).render(
     <StrictMode>
+        <PWAInstallBanner />
         <Backstage />
     </StrictMode>,
 )
