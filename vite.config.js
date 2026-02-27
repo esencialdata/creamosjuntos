93f2e7bc8b51037abb2c9eb2c18897664e739e54
Aaron Espinosa <aaron@esencial.work>
2026-02-27 10:06:08 -0600

    fix: increase PWA precache limit for entropia.png

--- vite.config.js
+++ vite.config.js
@@ -36,7 +36,7 @@ export default defineConfig({
       workbox: {
         skipWaiting: true,
         clientsClaim: true,
-        maximumFileSizeToCacheInBytes: 4000000,
+        maximumFileSizeToCacheInBytes: 5000000,
         globPatterns: ['**/*.{js,css,html,ico,png,svg,json}'],
         runtimeCaching: [
           {
